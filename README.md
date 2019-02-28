0528a87fe26f54e2d186d791c06bc2748abfd671
raymond-van <vraymond028@gmail.com>
2019-02-28 09:16:19 -0500

    Added pause functionality, fix bugs

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # pomodoro-app
 Pomodoro app built with React
->>>>>>> 413cd59e519c1ed076876331f0a02a4de1e8ab50
--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class TimerInput extends React.Component {
     return (
       <div>
         <h3>Set your work interval:</h3>
-        <input type="number" value={this.props.value} onChange={this.props.handleChange} required />
+        <input type="number" value={this.props.interval} step="5" onChange={this.props.handleChange} required />
       </div>
     );
   }
@@ -16,7 +16,7 @@ class Timer extends Component {
   render() {
     return (
       <div class='Timer'>
-        <h1>{this.props.value}:{this.props.seconds}</h1>
+        <h1>{this.props.minutes}:{this.props.seconds}</h1>
       </div>
     );
   }
@@ -26,7 +26,7 @@ class StartButton extends React.Component {
   render() {
     return (
       <div>
-        <button onClick={this.props.startCountdown}>Start</button>
+        <button onClick={this.props.startCountdown}>{this.props.control}</button>
       </div>
     );
   }
@@ -37,7 +37,9 @@ class App extends Component {
     super(props);
     this.state = {
       seconds: '00',
-      value: ''
+      minutes: '50',
+      interval: '50',
+      control: 'Start'
     };
     this.secondsRemaining = 0; 
     this.intervalHandle = null;
@@ -48,21 +50,37 @@ class App extends Component {
 
   handleChange(e) {
     this.setState({
-        value: e.target.value
+        minutes: e.target.value,
+        interval: e.target.value
     })
   }
 
   startCountdown() {
-    this.intervalHandle = setInterval(this.tick, 1000);
-    let time = this.state.value;
-    this.secondsRemaining = time * 60;
+    if (this.state.control === 'Start') {
+      this.intervalHandle = setInterval(this.tick, 1000);
+      let time = this.state.minutes;
+      this.secondsRemaining = time * 60 + parseInt(this.state.seconds);
+      this.setState({
+        control: 'Pause'
+      });
+    }
+    if (this.state.control === 'Pause') {     
+      clearInterval(this.intervalHandle); 
+      var min = Math.floor(this.secondsRemaining / 60);
+      var sec = this.secondsRemaining - (min * 60);
+      this.setState({
+        minutes: min,
+        seconds: sec,
+        control: 'Start'
+      });
+    }
   }
 
   tick() {
     var min = Math.floor(this.secondsRemaining / 60);
     var sec = this.secondsRemaining - (min * 60);
     this.setState({
-      value: min,
+      minutes: min,
       seconds: sec
     })
     if (sec < 10) {
@@ -72,7 +90,7 @@ class App extends Component {
     }
     if (min < 10) {
     this.setState({
-      value: "0" + min
+      minutes: "0" + min
      })
     }
     if (min === 0 & sec === 0) {
@@ -84,9 +102,9 @@ class App extends Component {
   render() {
     return (
       <div>
-        <TimerInput value={this.state.value} handleChange={this.handleChange}/>
-        <Timer value={this.state.value} seconds={this.state.seconds}/>
-        <StartButton startCountdown={this.startCountdown}/>
+        <TimerInput interval={this.state.interval} handleChange={this.handleChange}/>
+        <Timer minutes={this.state.minutes} seconds={this.state.seconds}/>
+        <StartButton startCountdown={this.startCountdown} control={this.state.control}/>
       </div>
     );
   }
